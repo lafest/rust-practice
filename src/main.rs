33354885d4451a298f29c69eb8f8121588af78ec
lafest <lafest@minding.today>
2021-06-10 21:47:57 +0900

    implement celsius-fahrenheit converter

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,14 @@
-extern crate rand;
-
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-
 fn main() {
-  println!("guess the number");
-  let secret_number = rand::thread_rng().gen_range(0..101);
-  loop {
-    println!("input your guess");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("failed to read line");
-    let guess: u32 = match guess.trim().parse() {
-      Ok(num) => num,
-      Err(_) => continue,
-    };
-  
-    println!("you guessed: {}", guess);
-  
-    match guess.cmp(&secret_number) {
-      Ordering::Less => println!("Less"),
-      Ordering::Greater => println!("Greater"),
-      Ordering::Equal => {
-        println!("Equal");
-        break;
-      },
-    }
-  }
+  let fahrenheit: f64 = 100.0;
+  let celsius: f64 = 36.5;
+  println!("fahrenheit: {} / converted: {}", fahrenheit, fahrenheit_to_celsius(fahrenheit));
+  println!("celsius: {} / converted: {}", celsius, celsius_to_fahrenheit(celsius));
+}
 
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+  celsius * 1.8 + 32.0
+}
+
+fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
+  (fahrenheit - 32.0) / 1.8
 }
